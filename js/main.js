01ff8fcf5f3e2377961f40072fbe1c734714913a
Omar de Paiva <omar.paiva@gmail.com>
2023-01-11 09:00:04 -0300

    criando elemento

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 const form = document.getElementById("novoItem");
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
-  console.log("funcionou");
+
+  criaElemento(
+    evento.target.elements["nome"].value,
+    evento.target.elements["quantidade"].value
+  );
 });
+
+function criaElemento(nome, quantidade) {
+  console.log(nome);
+  console.log(quantidade);
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = quantidade;
+  novoItem.innerHTML = numeroItem + nome;
+  console.log(novoItem);
+}
