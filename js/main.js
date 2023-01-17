9347d28cbcf5162298a36e2631bbbdb2c82842e7
Omar de Paiva <omar.paiva@gmail.com>
2023-01-17 18:34:43 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -11,15 +11,23 @@ form.addEventListener("submit", (evento) => {
 
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
+
   const existe = itens.find((elemento) => elemento.nome === nome.value);
+
   const itemAtual = {
     nome: nome.value,
     quantidade: quantidade.value,
   };
+
   if (existe) {
     itemAtual.id = existe.id;
+
+    atualizaElemento(itemAtual);
+
+    itens[existe.id] = itemAtual;
   } else {
     itemAtual.id = itens.length;
+
     criaElemento(itemAtual);
 
     itens.push(itemAtual);
@@ -38,9 +46,14 @@ function criaElemento(item) {
   const numeroItem = document.createElement("strong");
   numeroItem.innerHTML = item.quantidade;
   numeroItem.dataset.id = item.id;
-
   novoItem.appendChild(numeroItem);
+
   novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
 }
