3a525cca99de64b6c1cd8fcff69476f722800beb
Omar de Paiva <omar.paiva@gmail.com>
2023-01-17 18:48:23 -0300

    deletando um elemento

--- js/main.js
+++ js/main.js
@@ -49,11 +49,24 @@ function criaElemento(item) {
   novoItem.appendChild(numeroItem);
 
   novoItem.innerHTML += item.nome;
-
+  novoItem.appendChild(botaoDeleta());
   lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
   document.querySelector("[data-id='" + item.id + "']").innerHTML =
     item.quantidade;
+}
+
+function botaoDeleta() {
+  const elementoBotao = document.createElement("button");
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode);
+  });
+  elementoBotao.innerText = "x";
+  return elementoBotao;
+}
+
+function deletaElemento(tag) {
+  tag.remove();
 }
